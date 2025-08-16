5c83559c4128473ae2c4e9443862727bfacffba1
omkarg984 <omkargaikwad8783@gmail.com>
2025-08-16 20:49:43 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,9 +1,8 @@
 def find_missing_number(arr):
-    n = len(arr) + 1  # total numbers including the missing one
-    total_sum = n * (n + 1) // 2  # sum of first n natural numbers
+    n = len(arr) + 1  
+    total_sum = n * (n + 1) 
     return total_sum - sum(arr)
 
-# Accept array input from user
 arr = list(map(int, input("Enter the array elements separated by space: ").split()))
 
 print("Missing number is:", find_missing_number(arr))
