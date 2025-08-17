5c415d1dc3180fd63adb81336ef1b8eb8121f68a
omkarg984 <omkargaikwad8783@gmail.com>
2025-08-17 21:36:20 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,19 @@
+
+def findDuplicate(nums):
+    slow = nums[0]
+    fast = nums[0]
+
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+
+    return slow
+arr = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+print("Duplicate number is:", findDuplicate(arr))
