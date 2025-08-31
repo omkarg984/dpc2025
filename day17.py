41b508bd2e9a729f5f4c8f041b199ad5467e5da4
omkarg984 <omkargaikwad8783@gmail.com>
2025-08-31 10:15:36 +0530

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,13 @@
+def prime_factors(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
+
+num = int(input("Enter a positive integer: "))
+
+print("Prime factorization:", prime_factors(num))
